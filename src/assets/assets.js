5053a408bee3802541ae8f5c3708782257f67f3e
thathujan123 <nthathut@gamil.com>
2025-03-13 14:11:40 +0530

    final commit

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,3 +1,5 @@
+export const url = import.meta.env.VITE_BE_URL;
+
 import logo from './logo.png'
 import add_icon from './add_icon.png'
 import order_icon from './order_icon.png'
@@ -5,7 +7,6 @@ import profile_image from './profile_image.png'
 import upload_area from './upload_area.png'
 import parcel_icon from './parcel_icon.png'
 
-export const url = 'http://localhost:5000'
 export const currency = '$'
 
 export const assets ={
